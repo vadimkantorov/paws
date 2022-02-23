2408632989b4139e6e30154df9af7be9b058a1a9
Vadim Kantorov <vadimkantorov@gmail.com>
2022-02-23 17:41:27 +0100

    ...

--- poehali.py
+++ poehali.py
@@ -20,6 +20,7 @@ def setup(name, root, region, availability_zone, vpc_id = None, subnet_id = None
     root = os.path.expanduser(os.path.join(root, '.' + po))
     os.makedirs(root, exist_ok = True)
     ec2 = boto3.client('ec2', region_name = region)
+    iam = boto3.client('iam', region_name = region)
 
     if not os.path.exists(os.path.join(root, name + '.pem')):
         try:
@@ -31,7 +32,8 @@ def setup(name, root, region, availability_zone, vpc_id = None, subnet_id = None
         with open(os.path.join(root, name + '.pem'), 'w') as f:
             f.write(key_pair['KeyMaterial'])
         
-        os.chmod(os.path.join(root, name + '.pem'), 600)
+        #os.chmod(os.path.join(root, name + '.pem'), 600)
+        subprocess.call(['chmod', '600', os.path.join(root, name + '.pem')])
 
     if not vpc_id:
         vpc = (ec2.describe_vpcs(Filters = [dict(Name = 'tag:Name', Values = [po + '*'])])['Vpcs'] + [None])[0]
@@ -70,20 +72,39 @@ def setup(name, root, region, availability_zone, vpc_id = None, subnet_id = None
         ec2.create_volume(VolumeType = 'io1', MultiAttachEnabled = True, AvailabilityZone = availability_zone, Size = hot_disk_size_gb, Iops = iops, TagSpecifications = TagSpecifications('volume', name = name, suffix = 'experiments'))
 
     #policy_arn = [p for p in iam.list_policies(Scope = 'AWS', PathPrefix = '/service-role/')['Policies'] if p['PolicyName'] == 'AmazonEC2RoleforSSM'][0]['Arn']
-    #policy_document = dict(
-    #    Version = '2012-10-17',
-    #    Statement = dict(
-    #        Effect = 'Allow',
-    #        Principal = dict(Service = 'ec2.amazonaws.com'),
-    #        Action = 'sts:AssumeRole'
-    #    )
-    #)
-    #role_created = iam.create_role(RoleName = name, AssumeRolePolicyDocument = json.dumps(policy_document))
-    #role_attached = iam.attach_role_policy(RoleName = name, PolicyArn = policy_arn)
-    #instance_profile_created = iam.create_instance_profile(InstanceProfileName = name)
-    #role_added_to_instance_profile = iam.add_role_to_instance_profile(InstanceProfileName = name, RoleName = name)
 
-def micro(region, availability_zone, name, instance_type = 't2.micro', image_name = 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20210430', shutdown_after_init_script = False):
+    try:
+        iam.get_instance_profile(InstanceProfileName = name)
+    except iam.exceptions.NoSuchEntityException:
+        
+        trust_relationship_policy_document = dict(
+            Version = '2012-10-17',
+            Statement = dict(
+                Effect = 'Allow',
+                Principal = dict(Service = 'ec2.amazonaws.com'),
+                Action = 'sts:AssumeRole'
+            )
+        )
+
+        policy_document = dict(
+            Version = '2012-10-17',
+            Statement = [dict(
+                Effect = 'Allow',
+                Action = [
+                    'ec2:AttachVolume',
+                    'ec2:DetachVolume'
+                ],
+                Resource = ['arn:aws:ec2:*:*:volume/*', 'arn:aws:ec2:*:*:instance/*' ]
+            )]
+        )
+        
+        iam.create_role(RoleName = name, AssumeRolePolicyDocument = json.dumps(trust_relationship_policy_document))
+        policy_arn = iam.create_policy(PolicyName = name, PolicyDocument = json.dumps(policy_document))['Policy']['Arn']
+        iam.attach_role_policy(RoleName = name, PolicyArn = policy_arn)
+        iam.create_instance_profile(InstanceProfileName = name)
+        iam.add_role_to_instance_profile(InstanceProfileName = name, RoleName = name)
+
+def micro(region, availability_zone, name, instance_type = 't3.micro', image_name = 'ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20210430', shutdown_after_init_script = False):
     ec2 = boto3.client('ec2', region_name = region)
 
     vpc = (ec2.describe_vpcs(Filters = [dict(Name = 'tag:Name', Values = [po + '*'])])['Vpcs'] + [None])[0]
@@ -97,29 +118,33 @@ def micro(region, availability_zone, name, instance_type = 't2.micro', image_nam
     volume_id_experiments = ec2.describe_volumes(Filters = [dict(Name = 'tag:Name', Values = [N(name, 'experiments')])])['Volumes'][0]['VolumeId']
 
     init_script = '''#!/bin/bash -xe
-    INSTANCEID=$(curl -s http://http://169.254.169.254/latest/meta-data/instance-id)
-    mkdir ~/datasets ~/experiments
+    INSTANCEID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
     sudo apt update
     sudo apt install -y awscli
 
-    aws ec2 attach-volume --device /dev/xvdd --instance-id $INSTANCEID --volume-id $VOLUMEID1
-    aws ec2 attach-volume --device /dev/xvde --instance-id $INSTANCEID --volume-id $VOLUMEID2
+    aws ec2 attach-volume --region $REGION --device /dev/xvdd --instance-id $INSTANCEID --volume-id $VOLUMEID1
+    aws ec2 attach-volume --region $REGION --device /dev/xvde --instance-id $INSTANCEID --volume-id $VOLUMEID2
     
     aws ec2 wait volume-in-use --volume-ids $VOLUMEID1 $VOLUMEID2
+    
+    #aws ec2 detach-volume --region $REGION --device /dev/xvdd --instance-id $INSTANCEID --volume-id $VOLUMEID1
+    #aws ec2 detach-volume --region $REGION --device /dev/xvde --instance-id $INSTANCEID --volume-id $VOLUMEID2
+    
+    '''.replace('$REGION', region).replace('$VOLUMEID1', volume_id_datasets).replace('$VOLUMEID2', volume_id_experiments)
 
-    [ "$(file -b -s /dev/xvdd)" == "data" ] && mkfs -t xfs /dev/xvdd
-    [ "$(file -b -s /dev/xvde)" == "data" ] && mkfs -t xfs /dev/xvde
+    #[ "$(file -b -s /dev/xvdd)" == "data" ] && mkfs -t xfs /dev/xvdd
+    #[ "$(file -b -s /dev/xvde)" == "data" ] && mkfs -t xfs /dev/xvde
     
-    mount /dev/xvdd ~/datasets
-    mount /dev/xvde ~/experiments
-    
-    '''.replace('$VOLUMEID1', volume_id_datasets).replace('$VOLUMEID2', volume_id_experiments)
+    #mkdir ~/datasets ~/experiments
+    #sudo mount /dev/xvdd ~/datasets
+    #sudo mount /dev/xvde ~/experiments
+    #sudo umount /dev/xvdd /dev/xvde
 
     if shutdown_after_init_script:
-        init_script += 'shutdown'
+        init_script += 'sudo shutdown'
 
-    print(ec2.run_instances(
+    instance = ec2.run_instances(
         InstanceType = instance_type, 
         ImageId = image_id, 
         MinCount = 1, 
@@ -128,16 +153,13 @@ def micro(region, availability_zone, name, instance_type = 't2.micro', image_nam
         Placement = dict(AvailabilityZone = availability_zone), 
         NetworkInterfaces = [dict(DeviceIndex = 0, Groups = [security_group['GroupId']], SubnetId = subnet['SubnetId'], AssociatePublicIpAddress = True)], 
         TagSpecifications = TagSpecifications('instance', name = name, suffix = 'micro'),
-        
-        #IamInstanceProfile = dict(Name = name), 
-        #UserData = init_script,
-    ))
+        IamInstanceProfile = dict(Name = name), 
+        UserData = init_script,
+    )
+    print('Instance launched', instance['Instance']['InstanceId'])
 
 def gpu(name, root, region, availability_zone):
     pass
-
-def datasets(name, root, region, availability_zone):
-    micro(region, availability_zone, name, shutdown_after_init_script = True)
 
 def ps(region, name, root):
     root = os.path.expanduser(os.path.join(root, '.' + po))
@@ -146,7 +168,7 @@ def ps(region, name, root):
     instances = [instance for reservation in ec2.describe_instances()['Reservations'] for instance in reservation['Instances']]
     for instance in instances:
         instance_name = ([tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name'] + ['NoName'])[0]
-        if not instance_name.startswith('poehali_'):
+        if not instance_name.startswith(po):
             continue
 
         state = instance['State']
@@ -164,13 +186,14 @@ def ps(region, name, root):
 
 def kill(region, instance_id):
     ec2 = boto3.client('ec2', region_name = region)
-    client.terminate_instances(InstanceIds = [instance_id])
+    print(ec2.terminate_instances(InstanceIds = [instance_id]))
     
 def killall(region, name = None):
     ec2 = boto3.client('ec2', region_name = region)
-    instances = [instance for reservation in ec2.describe_instances(Filters = [dict(Name = 'tag:Name', Values = [N(name = name) + '_*'])]) ['Reservations'] for instance in reservation['Instances']]
-    print(instances)
-    #client.terminate_instances(InstanceIds = [instance['InstanceId'] for instance in instances])
+    instances = [instance for reservation in ec2.describe_instances(Filters = [dict(Name = 'instance-state-name', Values = ['running'] ), dict(Name = 'tag:Name', Values = [N(name = name) + '_*'])]) ['Reservations'] for instance in reservation['Instances']]
+    instance_ids = [instance['InstanceId'] for instance in instances]
+    ec2.terminate_instances(InstanceIds = instance_ids)
+    print('Terminated', instance_ids)
 
 def ssh(region, name, root, instance_id, username = 'ubuntu'):
     root = os.path.expanduser(os.path.join(root, '.' + po))
@@ -181,7 +204,6 @@ def ssh(region, name, root, instance_id, username = 'ubuntu'):
         print('Instance [', instance_id, '] not found')
         return
     
-    print(instance)
     public_ip = instance.get('PublicIpAddress')
     if not public_ip:
         print('Instance [', instance_id, '] does not have public IP')
@@ -191,14 +213,18 @@ def ssh(region, name, root, instance_id, username = 'ubuntu'):
     print(' '.join(cmd))
     print()
 
+    subprocess.call(['chmod', '600', os.path.join(root, name + '.pem')])
     subprocess.call(cmd)
 
 def help(region):
-    print('VPCs @', f'https://console.aws.amazon.com/vpc/home?region={region}#vpcs')
-    print('Subnets @', f'https://console.aws.amazon.com/vpc/home?region={region}#subnets')
-    print('Keypairs @', f'https://console.aws.amazon.com/ec2/v2/home?region={region}#KeyPairs')
-    print('Instances @', f'https://console.aws.amazon.com/ec2/v2/home?region={region}#Instances')
-    print('EBS disks @', f'https://console.aws.amazon.com/ec2/v2/home?region={region}#Volumes')
+    print(f'https://console.aws.amazon.com/vpc/home?region={region}#vpcs')
+    print(f'https://console.aws.amazon.com/vpc/home?region={region}#subnets')
+    print(f'https://console.aws.amazon.com/ec2/v2/home?region={region}#KeyPairs')
+    print(f'https://console.aws.amazon.com/ec2/v2/home?region={region}#Instances')
+    print(f'https://console.aws.amazon.com/ec2/v2/home?region={region}#Volumes')
+
+def datasets(name, root, region, availability_zone):
+    micro(region, availability_zone, name, shutdown_after_init_script = True)
 
 def ls(region, name):
     ec2 = boto3.client('ec2', region_name = region)
@@ -222,7 +248,7 @@ if __name__ == '__main__':
     parser.add_argument('--availability-zone', default = 'us-east-1a')
     parser.add_argument('--cold-disk-size-gb', type = int, default = 50)
     parser.add_argument('--hot-disk-size-gb', type = int, default = 50)
-    parser.add_argument('--name', default = 'poehalitest43')
+    parser.add_argument('--name', default = 'poehalitest44')
     parser.add_argument('--root', default = '~')
     parser.add_argument('--vpc-id')
     parser.add_argument('--subnet-id')
